d0be741cc2691ff095395e919604bbf50fcad68d
TomaszSteblik <tsteblik@gmail.com>
2022-02-24 22:34:04 +0100

    added di services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(builder.Configuration.GetConnectionString("mongoDb")));
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
